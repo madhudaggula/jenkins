5e5acc21c67d8b865c8f724c2b17a68ae0543291
madhudaggula <daggulamadhu33@gmail.com>
2026-02-12 11:57:36 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,6 @@ public class HelloWorld {
     {
         System.out.println("Hello, World");
         System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
+
     }
 }
